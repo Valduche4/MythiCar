cc04036d4a322fc1b26204bf6bb2fc902fb825d5
Valentin de Laminne <valentindelaminne@hotmail.be>
2022-05-31 16:01:53 +0200

    garagres index pages with mapbox

--- app/controllers/garages_controller.rb
+++ app/controllers/garages_controller.rb
@@ -1,5 +1,13 @@
 class GaragesController < ApplicationController
   def index
     @garages = Garage.all
+
+    @markers = @garages.geocoded.map do |garage|
+      {
+        lat: garage.latitude,
+        lng: garage.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { garage: garage })
+      }
+    end
   end
 end
