8d785b920c8c4e643e804c1026444035b92e06ea
Valentin de Laminne <valentindelaminne@hotmail.be>
2022-06-02 11:05:53 +0200

    marker working

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -23,18 +23,18 @@ export default class extends Controller {
       const popup = new mapboxgl.Popup().setHTML(marker.info_window)
 
       const customMarker = document.createElement("div")
-    // customMarker.className = "marker"
-    // customMarker.style.backgroundImage = `url('${marker.image_url}')`
-    // customMarker.style.backgroundSize = "contain"
-    // customMarker.style.width = "25px"
-    // customMarker.style.height = "25px"
+      customMarker.className = "marker"
+      customMarker.style.backgroundImage = `url('${marker.image_url}')`
+      customMarker.style.backgroundSize = "contain"
+      customMarker.style.width = "25px"
+      customMarker.style.height = "25px"
+      // customMarker.style.backgroundColor
 
-    // customMarker ----->
-      new mapboxgl.Marker()
+      new mapboxgl.Marker(customMarker)
         .setLngLat([ marker.lng, marker.lat ])
         .setPopup(popup)
         .addTo(this.map)
-    });
+    })
   }
 
   #fitMapToMarkers() {
